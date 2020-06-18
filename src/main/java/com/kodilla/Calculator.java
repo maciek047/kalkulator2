db31800cb1b7788abb6e7640aa8e731ff2d0173a
maciek047 <maciek.047@gmail.com>
2020-06-18 05:37:36 +0200

    2nd Commit

--- src/main/java/com/kodilla/Calculator.java
+++ src/main/java/com/kodilla/Calculator.java
@@ -8,5 +8,10 @@ public class Calculator {
     public int odejmowanie(int x1, int x2) {
         return x1 - x2;
     }
+
+    public static void main(String args[]){
+        Calculator calculator = new Calculator();
+        System.out.println("Wynik dodawania: " + calculator.dodawanie(7,8));
     }
 }
+
