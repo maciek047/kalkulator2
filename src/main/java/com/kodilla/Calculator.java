1e10c7b255eab0534d1a75b76eb64deca9d4394f
maciek047 <maciek.047@gmail.com>
2020-06-18 05:23:52 +0200

    First Commit

---
+++ src/main/java/com/kodilla/Calculator.java
@@ -0,0 +1,12 @@
+package com.kodilla;
+
+public class Calculator {
+    public int dodawanie(int x1, int x2) {
+        return x1 + x2;
+    }
+
+    public int odejmowanie(int x1, int x2) {
+        return x1 - x2;
+    }
+    }
+}
